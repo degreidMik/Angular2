d692bb34893ed7ac62eaaec3d44b6abc860b5a19
v.mikhaylin <hackdeg@gmail.com>
2016-09-19 16:30:08 +0300

    2. Master/Detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
    
     selector: 'my-app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
 export class AppComponent  {
-   hero: Hero = {
-  id: 1,
-  name: 'Windstorm'
-};
-  
+
+  heroes: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+selectedHero: Hero;
+
+onselect(hero: Hero): void {
+    this.selectedHero = hero;
+}
+
 }
 
 export class Hero {
